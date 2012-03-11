d20b20a24fbe2b851467868f4572a92b383bc59d
Aliaksandr Palanetski <debrazer@gmail.com>
2012-03-11 18:52:33 +0300

    script to find fragmented files

---
+++ find_fragmented_files/listfrag.rb
@@ -0,0 +1,20 @@
+def get_extents file_name
+	`filefrag "#{file_name}"`.scan(/\d+/).last.to_i
+end
+
+files = []
+Dir.glob("**/*") do |file_name|
+	next if File.directory? file_name
+	
+	files << {:name => file_name, 
+		:extents => get_extents(file_name),
+		:size => File.size(file_name)
+		}
+end
+
+puts "extents\tsize\tfile"
+files.sort {|a1,a2| a2[:extents] <=> a1[:extents]}.each do |f|
+	puts f[:extents].to_s + "\t" + f[:size].to_s + "\t" + f[:name]
+end
+
+
